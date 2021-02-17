17acb4f1d0bfe0913706d540162fe94fcece6c35
brocktubre <brocktubre@gmail.com>
2021-02-17 14:53:23 -0500

    URL error checking.

--- src/app/oss-coding/oss-code/oss-code.component.ts
+++ src/app/oss-coding/oss-code/oss-code.component.ts
@@ -15,10 +15,13 @@ export class OssCodeComponent implements OnInit {
   public userLogin: string;
   public body: string;
   public commentUrl: string;
+  public commentId: number;
   public startingComment: number;
   public endingComment: number;
   public currentComment: number;
   public year: number;
+  public hideButtons: boolean;
+  public userMessage: string;
 
   constructor(private activeRoute: ActivatedRoute, private ossCodingService: OssCodingService, private router: Router) {
     this.year = new Date().getFullYear();
@@ -27,6 +30,7 @@ export class OssCodeComponent implements OnInit {
 
   ngOnInit(): void {
     this.loadingComments = true;
+    this.hideButtons = false;
     this.allComments = this.ossCodingService.getAllCommentsSingleton();
 
     if(this.allComments === undefined || this.allComments.length == 0) {
@@ -34,25 +38,52 @@ export class OssCodeComponent implements OnInit {
       this.ossCodingService.getAllComments().subscribe((comments) => {
           this.allComments = comments;
           this.loadingComments = false;
+          let startVal = Number(this.activeRoute.snapshot.params['start']);
           this.endingComment = Number(this.activeRoute.snapshot.params['end']);
           this.currentComment = Number(this.activeRoute.snapshot.params['current']);
           this.startingComment = this.currentComment;
+
+          if(this.currentComment == this.endingComment) {
+            this.hideButtons = true;
+            this.userMessage = 'All done coding your selected comments!';
+          }
+
+          if(this.endingComment < startVal || this.currentComment < startVal
+            || this.currentComment > this.endingComment) {
+            this.router.navigate(['/oss-coding/']);
+            return;
+          }
 
           let firstComment = this.allComments[this.startingComment];
           this.userLogin = firstComment.user_login;
           this.body = firstComment.body;
           this.commentUrl = firstComment.html_url;
+          this.commentId = firstComment.id;
       });
     } else {
       this.loadingComments = false;
+      this.hideButtons = false;
       this.endingComment = Number(this.activeRoute.snapshot.params['end']);
       this.currentComment = Number(this.activeRoute.snapshot.params['current']);
       this.startingComment = this.currentComment;
+      let startVal = Number(this.activeRoute.snapshot.params['start']);
+
+      if(this.currentComment == this.endingComment) {
+        this.hideButtons = true;
+        this.userMessage = 'All done coding your selected comments!';
+      }
+
+      if(this.endingComment < startVal || this.currentComment < startVal
+        || this.currentComment > this.endingComment) {
+        this.router.navigate(['/oss-coding/']);
+        return;
+      }
 
       let firstComment = this.allComments[this.startingComment];
       this.userLogin = firstComment.user_login;
       this.body = firstComment.body;
       this.commentUrl = firstComment.html_url;
+      this.commentId = firstComment.id;
     }
   }
 
